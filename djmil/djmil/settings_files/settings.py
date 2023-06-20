67b778d6451432a1d7f315464f0e395604430e2a
priest <ami8800@gmail.com>
2023-06-20 12:28:54 +0300

    version 1.9.7
    1) improve logic on second_online_order
    2) add pagination in second_online order
    3) add without fake drone filtr
    4) improve velocity and stability

--- djmil/djmil/settings_files/settings.py
+++ djmil/djmil/settings_files/settings.py
@@ -75,10 +75,10 @@ WSGI_APPLICATION = 'djmil.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': "vidma",
-        'USER': 'djmil_admin',
-        'PASSWORD': 'djmil67dev',
-        'HOST': '85.209.89.166',
+        'NAME': "",
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
         'PORT': '5432',
     }
 }
